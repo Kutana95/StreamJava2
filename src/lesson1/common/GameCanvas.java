d4a863cdc23892e4c4c957061a480724a2a4c255
kutana <anechkakutovaya@mail.ru>
2019-07-21 23:45:02 +0700

    done lesson1

---
+++ src/lesson1/common/GameCanvas.java
@@ -0,0 +1,36 @@
+package lesson1.common;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameCanvas extends JPanel {
+
+    CanvasListener gameWindow;
+    private long lastFrameTime;
+
+    public GameCanvas(CanvasListener gameWindow) {
+        this.gameWindow = gameWindow;
+        lastFrameTime = System.nanoTime();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        long currentTime = System.nanoTime();
+        float delta = (currentTime - lastFrameTime) * 0.000000001f;
+        lastFrameTime = currentTime;
+        try {
+            Thread.sleep(17);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        gameWindow.onDrawFrame(this, g, delta);
+        repaint();
+    }
+
+    public int getLeft() { return 0; }
+    public int getRight() { return getWidth() - 1; }
+    public int getTop() { return 0; }
+    public int getBottom() { return getHeight() - 1; }
+}
+
